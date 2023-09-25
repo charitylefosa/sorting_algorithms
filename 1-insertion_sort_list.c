3d5edffcc267fcca64f203c03f2cd3adbacd7ca8
MphoCM <christian.mofokeng@gmal.com>
2023-09-25 13:24:38 +0000

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,51 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linkked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev =(*n1)-prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next + n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
-
-/**
- * isertion_sort_list - Sort a doubly linked listof inteers
- * 			using the insertion sort algorithm.
- * @list: A pointer to the head of head of a doubly-linked list of integers.
- *
- * Descroption: Prints the list after each swap.
+ * insertion_sort_list - sorts doubly linked list of ints
+ *                in ascending order using insertion algo
+ * @list: doubly linked list to sort
+ * Return: nothing
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *temp, *pre, *current;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+
+	current = (*list)->next;
+	temp = current->next;
+	while (current)
 	{
-		tmp = iter->next;
-		insert =miter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		if (current->n < current->prev->n)
 		{
-			swap_nodes(list, &insert, iter);
-			print_list((const listint_t *)*list);
+			pre = current->prev;
+			while (pre && (current->n < pre->n))
+			{
+				if (!(pre->prev))
+				{
+					pre->prev = current;
+					current->prev->next = current->next;
+					if (current->next)
+						current->next->prev = current->prev;
+					current->next = pre;
+					current->prev = NULL;
+					*list = current;
+				} else
+				{
+					current->prev->next = current->next;
+					if (current->next)
+						current->next->prev = current->prev;
+					pre->prev->next = current;
+					current->prev = pre->prev;
+					pre->prev = current;
+					current->next = pre;
+				}
+				print_list(*list);
+				pre = current->prev;
+			}
 		}
+		current = temp;
+		current ? (temp = current->next) : (temp = NULL);
 	}
 }
